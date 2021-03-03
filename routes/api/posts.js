6c914df38c6ad7b68bfb093f4a8c840d50ea6199
Robert_Norwood <robnorwood91@gmail.com>
2021-03-03 10:04:59 -0800

    Able to get all posts, get post by id, and delete a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -34,4 +34,60 @@ router.post(
   }
 );
 
+//GET ALL POSTS, Private
+router.get('/', auth, async (req, res) => {
+  try {
+    //Finds all posts and sorts them by newest first (using date: -1 does this)
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//GET POST BY ID, Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+//DELETE A POST, Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    //Make sure the correct user can delete the post
+    //We have to add the toString to make this a string so that the logic can correctly compare the post.user to the req.user.id
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await post.remove();
+
+    res.json({ msg: 'Post successfully removed' });
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
