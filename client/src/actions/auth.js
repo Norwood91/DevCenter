1f11bfae469205fc1908fc9b45ff90cc59759a3c
Robert_Norwood <robnorwood91@gmail.com>
2021-03-06 13:53:38 -0800

    Able to log a user in via the UI and redirect to dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,8 @@ import {
   REGISTER_FAIL,
   AUTH_ERROR,
   USER_LOADED,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
 } from './types';
 
 //LOAD A USER
@@ -34,6 +36,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
   try {
     const res = await axios.post('/api/users', body, config);
     dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    dispatch(loadUser());
   } catch (err) {
     //The array of errors in the server is called errors
     const errors = err.response.data.errors;
@@ -41,5 +44,28 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
     dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+//LOGIN A USER
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (err) {
+    //The array of errors in the server is called errors
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({ type: LOGIN_FAIL });
   }
 };
