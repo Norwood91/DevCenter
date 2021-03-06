d7de56816da0e87907bf634ad785f64ee9fa55a3
Robert_Norwood <robnorwood91@gmail.com>
2021-03-06 13:30:58 -0800

    Able to now load user and authenticate with registration

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,26 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  AUTH_ERROR,
+  USER_LOADED,
+} from './types';
+
+//LOAD A USER
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+    dispatch({ type: USER_LOADED, payload: res.data });
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR });
+  }
+};
 
 //REGISTER A USER
 export const register = ({ name, email, password }) => async (dispatch) => {
