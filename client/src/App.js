0f157018513aec8068d745f61f498b87e6a28866
Robert_Norwood <robnorwood91@gmail.com>
2021-03-08 13:10:27 -0800

    User now able to add job experience and education to their personal profile

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 //REDUX
 import { Provider } from 'react-redux';
@@ -45,6 +47,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
               />
             </Switch>
           </section>
