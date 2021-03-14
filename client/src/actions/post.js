bb6692158e5d243f9a8f0b2d1ee760df64af285a
Robert_Norwood <robnorwood91@gmail.com>
2021-03-14 03:54:57 -0700

    Able to delete a post now

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { setAlert } from '../actions/alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -40,6 +40,22 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+    dispatch(setAlert('Post Successfully Removed', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
