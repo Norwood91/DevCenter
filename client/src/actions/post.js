0dc3edcfb85abc4f2ee04c332cfdfae0eaf33b8d
Robert_Norwood <robnorwood91@gmail.com>
2021-03-14 03:45:05 -0700

    Able to like and unlike a post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { setAlert } from '../actions/alert';
-import { GET_POSTS, POST_ERROR, PROFILE_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -9,6 +9,36 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
     });
   } catch (err) {
     dispatch({
